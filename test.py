365052b77bcadd23ad071bd445e8ec68f51b7554
Youcef k <youssefkaddour@hotmail.com>
2024-01-16 12:24:11 +0100

    commit 37

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import os
+from numpy import log
+
 print("bon on essayer hein")
 
 a = os.environ['A']
 b = os.environ['B']
-print(a, b)
+# Tache 4
+print('la multiplication : ', a * b)
+
+# BONUS
+log1 = log(a*b)
+log2 = log(a) + log(b)
+
+print('BONUS :')
+print('log(a*b) = ', log1)
+print('log(a) + log(b) = ', log2)
+
+print('hamdoulilah')
